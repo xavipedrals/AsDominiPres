35eb38dfbb61cdb2f205378a38a3356ebdb68a22
Marcos Pérez Rubio <marcos.perez@est.fib.upc.edu>
2015-06-15 14:03:27 +0200

    Arreglat IObtenirRanking

--- src/com/company/Domini/EstrategiaRanking/MillorPuntuacio.java
+++ src/com/company/Domini/EstrategiaRanking/MillorPuntuacio.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class MillorPuntuacio implements IEstrategiaRanking {
 
     @Override
-    public ArrayList<Pair> obtenirRanking(Partida p) {
+    public ArrayList<Pair> obtenirRanking() {
         //Aqui aniria el recorrer les taules per a cada jugador
         //S'ha de cridar a CtrlJugador i a la funció tots que retorna un arraylist, a partir d'aquí es va fent
 
