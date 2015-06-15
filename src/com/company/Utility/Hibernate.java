c0bdc590874a30814ee0ecfe86268a07777781cf
Marcos Pérez Rubio <marcos.perez@est.fib.upc.edu>
2015-06-15 10:53:20 +0200

    Hibernate i postresql afegit

--- src/com/company/Utility/Hibernate.java
+++ src/com/company/Utility/Hibernate.java
@@ -4,4 +4,15 @@ package com.company.Utility;
  * Created by marcos on 15/06/2015.
  */
 public class Hibernate {
+    //private static final SessionFactory session = buildSession();
+
+    /*
+
+
+
+
+
+
+
+     */
 }
