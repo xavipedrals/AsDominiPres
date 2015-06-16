5b4b92ad069afa5aeadd89682cb1dee54323885c
Esteve Torrents Lapuerta <esteve.torrents@est.fib.upc.edu>
2015-06-16 02:56:20 +0200

    view ranking

--- src/com/company/Presentacio/Views/RankingView.java
+++ src/com/company/Presentacio/Views/RankingView.java
@@ -1,11 +1,41 @@
 package com.company.Presentacio.Views;
 
+import com.company.Utility.Pair;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
 /**
  * Created by marcos on 16/06/2015.
  */
 public class RankingView extends JugarPartidaTemplate{
 
 
+    private JPanel panel1;
+    private JList list1;
+    private JList list2;
+    private JButton menuButton;
+    private ArrayList<Pair> result;
 
+    private void insereixDades() {
+        ArrayList<Pair> r = new ArrayList<Pair>();
+        for (int i = 0; i < 5; ++i) {
+            Pair p = new Pair("Usuari"+i, i*35000);
+            result.add(p);
+        }
+    }
 
+    public RankingView(){
+        insereixDades();
+        DefaultListModel listModel1 = new DefaultListModel();
+        DefaultListModel listModel2 = new DefaultListModel();
+        list1 = new JList(listModel1);
+        list2 = new JList(listModel2);
+        for (Pair p : result) {
+            listModel1.addElement(p.user);
+            listModel2.addElement(p.puntuacio);
+        }
+
+        add(panel1);
+    }
 }
